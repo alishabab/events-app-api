28ea525f23a78e4af9ef47a5fbad19efedfad4ec
Shabab Ali <shababsaifi@gmail.com>
2020-12-15 17:38:01 +0530

    implemented jwt authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'login', to: 'authentication#login'
+  get 'auto_login', to: 'authentication#auto_login'
+  get 'user_is_authed', to: 'authentication#user_is_authed'
 end
